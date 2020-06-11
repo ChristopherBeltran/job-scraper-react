5c611b404cc3081a3132ab4a8c0c63cd52b73b88
Christopher Beltran <christod.beltran@gmail.com>
2020-06-11 17:18:34 -0600

    Adding procfile, and serverjs file

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+app.use(express.static(__dirname + '/'));
+app.listen(process.env.PORT || 8080);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'));
+    app.get('*', function (req, res) {
+        const index = path.join(__dirname, 'build', 'index.html');
+        res.sendFile(index);
+    });
+}
